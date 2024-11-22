7934e9685703c7328201e65ebeb9de5a627923cb
enmanuelmag <enmanuelmag@cardor.dev>
2024-11-21 19:59:45 -0500

    feat: add all form and get data

---
+++ src/content/form/index.ts
@@ -0,0 +1,11 @@
+export const WeddingType = ['Civil', 'Religiosa'];
+
+export const WishList = [
+  'Alquiler de Locación',
+  'Catering',
+  'Decoración',
+  'Fotografía',
+  'Videografía',
+  'DJ',
+  'Música en Vivo',
+];
