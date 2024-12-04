76876cad630616dfa3940bd690fce04a113e6267
enmanuelmag <enmanuelmag@cardor.dev>
2024-12-04 07:19:24 -0500

    feat: add new i18n in all home section

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -19,5 +19,14 @@ export const ui = {
       'Esse nisi fugiat ipsum deserunt culpa qui enim cillum sunt laboris addolore proident occaecat. Amet velit do deserunt velit laboris. Temporullamco deserunt ad velit laboris eiusmod reprehenderit. Voluptate utnulla excepteur cupidatat. Anim ad in voluptate consequat Lorem aliquipexercitation. Tempor irure dolor voluptate dolore esse tempor aliquip eaaliqua proident duis veniam quis do.',
     //SERVICES SECTION
     'services.title': 'Nuestros servicios',
+    //TESTIMONIALS SECTION
+    'testimonials.title': 'Testimonios',
+    'testimonials.subtitle':
+      'Esse nisi fugiat ipsum deserunt culpa qui enim cillum sunt laboris addolore proident occaecat. Amet velit do deserunt velit laboris. Temporullamco deserunt ad velit laboris eiusmod reprehenderit. Voluptate utnulla excepteur cupidatat. Anim ad in voluptate consequat Lorem aliquipexercitation. Tempor irure dolor voluptate dolore esse tempor aliquip eaaliqua proident duis veniam quis do.',
+    //FEATURES SECTION
+    'featuring.title':
+      'Gracias a mi servicio de organización integral de bodas boutique, experimentarás:',
+    'featuring.items':
+      'Enim cillum enim occaecat aute dolor laborum aute commodo occaecat nulla.|Enim ut deserunt exercitation ut excepteur. Velit eiusmod irure reprehenderit fugiat velit officia do dolor dolore amet consectetur pariatur.|Laboris fugiat laborum ad magna. Commodo excepteur magna enim non veniam labore cupidatat aute.|Nulla amet deserunt exercitation nulla culpa. Velit id culpa est ad enim quis Lorem amet officia do et.',
   },
 } as const;
