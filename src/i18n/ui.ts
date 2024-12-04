d1dad7cf833d986d4b235bdf3f1e96cc64c0d608
enmanuelmag <enmanuelmag@cardor.dev>
2024-12-03 20:08:21 -0500

    feat: new i18n hook

---
+++ src/i18n/ui.ts
@@ -0,0 +1,23 @@
+export const showDefaultLang = false;
+
+export const languages = {
+  sp: 'Español',
+};
+
+export const defaultLang = 'sp';
+
+export const ui = {
+  sp: {
+    //HERO SECTION
+    'hero.title':
+      'Pariatur ea eiusmod reprehenderit culpa<br />sit reprehenderit est esse',
+    'hero.subtitle': 'Ex Lorem sunt cillum sint eiusmod dolore culpa',
+    //DESCRIPTION SECTION
+    'description.title':
+      'Qui aliquip nulla cupidatat commodo excepteur exercitation',
+    'description.subtitle':
+      'Esse nisi fugiat ipsum deserunt culpa qui enim cillum sunt laboris addolore proident occaecat. Amet velit do deserunt velit laboris. Temporullamco deserunt ad velit laboris eiusmod reprehenderit. Voluptate utnulla excepteur cupidatat. Anim ad in voluptate consequat Lorem aliquipexercitation. Tempor irure dolor voluptate dolore esse tempor aliquip eaaliqua proident duis veniam quis do.',
+    //SERVICES SECTION
+    'services.title': 'Nuestros servicios',
+  },
+} as const;
