ba4b2a0bc212345071a4cde05ee8b2b90e2abc67
enmanuelmag <enmanuelmag@cardor.dev>
2025-01-08 15:20:00 -0500

    feat: remove POST temp

--- src/pages/api/email.json.ts
+++ src/pages/api/email.json.ts
@@ -2,24 +2,24 @@ export const prerender = false;
 
 import type { APIRoute } from 'astro';
 
-import { sendEmail } from '@utils/email';
+//import { sendEmail } from '@utils/email';
 
-export const POST: APIRoute = async ({ request, locals }) => {
-  try {
-    const formData = await request.json();
+// export const POST: APIRoute = async ({ request, locals }) => {
+//   try {
+//     const formData = await request.json();
 
-    await sendEmail(formData, {
-      resendApiKey: locals.runtime.env.RESEND_API_KEY,
-      emailFrom: locals.runtime.env.SEND_EMAIL_FROM,
-      emailTo: locals.runtime.env.SEND_EMAIL_TO,
-    });
+//     await sendEmail(formData, {
+//       resendApiKey: locals.runtime.env.RESEND_API_KEY,
+//       emailFrom: locals.runtime.env.SEND_EMAIL_FROM,
+//       emailTo: locals.runtime.env.SEND_EMAIL_TO,
+//     });
 
-    return new Response(JSON.stringify({ success: true }), { status: 200 });
-  } catch (error) {
-    console.error('Error sending email', error);
-    return new Response(JSON.stringify({ success: false }), { status: 500 });
-  }
-};
+//     return new Response(JSON.stringify({ success: true }), { status: 200 });
+//   } catch (error) {
+//     console.error('Error sending email', error);
+//     return new Response(JSON.stringify({ success: false }), { status: 500 });
+//   }
+// };
 
 export const GET: APIRoute = async () => {
   try {
